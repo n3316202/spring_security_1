4b31b63ab5586a3a0a5f2f787bb8636c9ae738f2
DESKTOP-KQPMITU\aiacademy <nqwrt@naver.com>
2021-06-08 12:13:50 +0900

    스텝 2완료

---
+++ src/main/java/edu/bit/ex/controller/SecurityController.java
@@ -0,0 +1,34 @@
+package edu.bit.ex.controller;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import lombok.extern.log4j.Log4j;
+
+
+@Log4j
+@RequestMapping("/security/*")
+@Controller
+public class SecurityController {
+	
+	@GetMapping("/all")
+	public void doAll() {
+		log.info("do all can access everybody");
+	}
+	
+	@GetMapping("/member")
+	public void doMember() {
+
+		log.info("logined member");
+	}
+	
+}
